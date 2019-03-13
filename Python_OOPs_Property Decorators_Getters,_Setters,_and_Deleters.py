9412b86e42d7848f3ad4feb186291132ad683434
iampaavan <46580953+iampaavan@users.noreply.github.com>
2019-03-13 19:09:24 -0400

    Special Methods - Getters, Setters and Deleters

---
+++ Python_OOPs_Property Decorators_Getters,_Setters,_and_Deleters.py
@@ -0,0 +1,38 @@
+class Employee:
+	
+	def __init__(self, first, last):
+		self.first = first
+		self.last = last
+	
+	@property
+	def email(self):
+		return '{}.{}@company.com'.format(self.first, self.last)
+	
+	@property
+	def fullname(self):
+		return '{} {}'.format(self.first, self.last)
+
+	@fullname.setter
+	def fullname(self, name):
+		first, last = name.split(' ')
+		self.first = first
+		self.last = last
+
+	@fullname.deleter
+	def fullname(self):
+		print('Delete Name! ')
+		self.first = None
+		self.last = None
+		
+		
+emp_1 = Employee('Paavan', 'Gopala')
+
+emp_1.fullname = 'Bhushan Gopala'
+
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
+
+del emp_1.fullname
+print(emp_1.first)
+
